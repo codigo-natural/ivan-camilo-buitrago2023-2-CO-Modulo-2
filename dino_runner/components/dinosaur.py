40692d9b0a17c23eba2299e0c22b4b8dbc094c42
Code-Nature <codenature01001@gmail.com>
2023-04-01 17:43:33 -0500

    Second-Class-Extra

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -8,6 +8,24 @@ from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 # Definimos la clase Dinosaur que hereda de Sprite
 class Dinosaur(Sprite):
   # Establecemos algunas constantes para la posición y velocidad del dinosaurio
+  """
+    Clase que representa al dinosaurio del juego.
+
+    Hereda de la clase Sprite de Pygame para ser dibujado en la pantalla.
+
+    Atributos:
+        X_POS (int): posición horizontal del dinosaurio en la pantalla.
+        Y_POS (int): posición vertical del dinosaurio en la pantalla.
+        JUMP_SPEED (float): velocidad de salto del dinosaurio.
+
+        image (Surface): imagen actual del dinosaurio.
+        dino_rect (Rect): rectángulo de colisión del dinosaurio.
+        step_index (int): índice de paso actual del dinosaurio.
+        dino_run (bool): indica si el dinosaurio está corriendo.
+        dino_jump (bool): indica si el dinosaurio está saltando.
+        jump_speed (float): velocidad de salto actual del dinosaurio.
+        dino_duck (bool): indica si el dinosaurio está agachado.
+    """
   X_POS = 80
   Y_POS = 310
   JUMP_SPEED = 8.5
@@ -33,15 +51,19 @@ class Dinosaur(Sprite):
     elif self.dino_duck:
       self.duck()
 
+    # Cambiamos al estado de salto si el usuario presiona la tecla de arriba y no estamos saltando actualmente
     if user_input[pygame.K_UP] and not self.dino_jump:
       self.dino_run = False
       self.dino_jump = True
+    # Cambiamos al estado de correr si el usuario no está presionando la tecla de arriba y no estamos saltando
     elif not self.dino_jump:
       self.dino_run = True
 
+    # Reiniciamos el índice de paso cada 10 ciclos de actualización
     if self.step_index >= 10:
       self.step_index = 0
 
+    # Cambiamos al estado de agacharse si el usuario presiona la tecla de abajo y no estamos saltando actualmente
     if user_input[pygame.K_DOWN] and not self.dino_jump:
       self.dino_duck = True
     else:
@@ -50,12 +72,14 @@ class Dinosaur(Sprite):
   def run(self):
     # Actualizamos la imagen del dinosaurio en el estado de correr
     if self.dino_duck:
+      # Cambiamos entre dos imágenes para crear la animación de agacharse
       self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
       self.dino_rect = self.image.get_rect()
       self.dino_rect.x = self.X_POS
       self.dino_rect.y = self.Y_POS + 25
       self.step_index += 1
     else:
+      # Cambiamos entre dos imágenes para crear la animación de correr
       self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
       self.dino_rect = self.image.get_rect()
       self.dino_rect.x = self.X_POS
